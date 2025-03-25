4aa8a219b38d8b69e4feffd63fb7c4e4e8efd063
Wendy Alfaro <200550011+Yanelly16@users.noreply.github.com>
2025-03-25 05:59:47 +0000

    Added Routes

--- task-management-app/config/db.js
+++ task-management-app/config/db.js
@@ -1,1 +1,2 @@
 //Filename: config/db.js
+
--- task-management-app/routes/taskmanagementRoutes.js
+++ task-management-app/routes/taskmanagementRoutes.js
@@ -1,1 +1,16 @@
 //Filename: routes/taskmanagementRoutes.js
+import express from "express";
+const router = express.Router();
+import {
+    getAllTasks,
+    createTask,
+    toggleTaskComplete,
+    deleteTask,     
+  } from "../controllers/subscriberController.js";
+  
+router.get('/', getAllTasks);
+router.post('/tasks',createTask);
+router.patch('/tasks/:id', toggleTaskComplete);
+router.delete('/tasks/:id', deleteTask);
+
+module.exports = router;
