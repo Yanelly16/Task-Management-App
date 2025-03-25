10fe9852912270be4d8f29495160f77ea1a73550
Wendy Alfaro <200550011+Yanelly16@users.noreply.github.com>
2025-03-25 19:56:04 +0000

    Database-Integration

--- task-management-app/config/db.js
+++ task-management-app/config/db.js
@@ -1,1 +1,34 @@
 //Filename: config/db.js
+
+import pg from 'pg';
+import dotenv from 'dotenv'; 
+dotenv.config();              // get the environment variables (.env)
+const { Pool } = pg; 
+
+// create a connection pool
+const pool = new Pool({ 
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+});
+
+// let's test the connection pool to ensure it actually works
+pool.connect((err, client, release) => { 
+    if (err) { 
+       return console.error('Error acquiring client', err.stack); 
+    } 
+    console.log('Connected to PostgreSQL database'); 
+    release();
+});
+
+// check for errors
+pool.on('error', (err) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
+
+// make variables available for importing from other files
+export const query = (text, params) => pool.query(text, params);
+export default pool;
