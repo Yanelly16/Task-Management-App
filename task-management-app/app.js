11ac6a1db491184e8bd2549689d2ac9167106657
Wendy Alfaro <200550011+Yanelly16@users.noreply.github.com>
2025-04-13 03:16:33 +0000

    Added Template partials

--- task-management-app/app.js
+++ task-management-app/app.js
@@ -35,7 +35,7 @@ app.use((req, res) => {
   res.status(404).send("404 Not Found.\n");
 });
 
-const PORT = 3019;
+const PORT = 2020;
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 });
